ba9050247be6028271e79cf32a869977cc1f774f
Alexander Korovin <3253340@gmail.com>
2019-01-16 02:04:21 +0700

    Refactor typo on example Refactor moxy example

--- app/src/main/java/com/example/rumpilstilstkin/lesson1/moxy/Model.java
+++ app/src/main/java/com/example/rumpilstilstkin/lesson1/moxy/Model.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Model{
+class Model{
     private List<Integer> mList;
 
-    public Model() {
+   Model() {
         mList = new ArrayList<>(3);
         mList.add(0);
         mList.add(0);
         mList.add(0);
     }
 
-    public int getElementValueAtIndex(int _index) {
+    int getElementValueAtIndex(int _index) {
         return mList.get(_index);
     }
 
-    public void setElementValueAtIndex(int _index, int value) {
+    void setElementValueAtIndex(int _index, int value) {
         mList.set(_index, value);
     }
 }
